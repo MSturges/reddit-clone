0c6efa5eb671f30638f690457c6344bf82847d6c
MSturges <Sturges3232@gmail.com>
2016-11-18 09:46:15 -0700

    header mvp complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Header from './header';
+
 export default class App extends Component {
 
   render() {
     return (
-      <div>
-      
+      <div className="app_container">
+        <Header/>
+
         {this.props.children}
 
       </div>
---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
+
+class Header extends Component {
+  test(){
+    console.log('hello');
+  }
+  render(){
+
+
+    return( <div className="header-container">
+      <div className="sub-reddit-bar"></div>
+      <div className="header">
+        <div className="header-image" ></div>
+
+        <div className="user-auth">
+        <span>Want to join?
+          <a onClick={this.test.bind(this)}> Login or Sign Up </a>
+          in seconds.
+        </span>
+
+        </div>
+
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
